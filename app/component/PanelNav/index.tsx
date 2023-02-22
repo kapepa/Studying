ec32eae4ccebb87d3a3cd2f2180a83d9270d8cf5
Vladimir <kapepayx@gmail.com>
2023-02-22 21:32:23 +0200

    app: btn, palen

--- app/component/PanelNav/index.tsx
+++ app/component/PanelNav/index.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
-import { FC } from "react";
+import React, { FC } from "react";
 import BtnDefault from "../BtnDefault";
 import styles from "./styles.module.scss";
 
 const PanelNav: FC = () => {
+
+  const onLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Login")
+  }
+
+  const onSignIn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('Sign in')
+  }
+
+
   return (
     <div className={styles.panel}>
       <div className="container">
@@ -11,17 +21,17 @@ const PanelNav: FC = () => {
           <Link href="/home">
             <div className={styles.panel__login}></div>
           </Link>
-          <div className="">
-            <nav className="">
-              <Link href={"/home"}>Home</Link>
-              <Link href={"/courses"}>Courses</Link>
-              <Link href={"/careers"}>Careers</Link>
-              <Link href={"/blog"}>Blog</Link>
-              <Link href={"/about"}>About Us</Link>
+          <div className={styles.panel__interaction}>
+            <nav className={styles.panel__nav}>
+              <Link className={styles.panel__link} href={"/home"}>Home</Link>
+              <Link className={styles.panel__link} href={"/courses"}>Courses</Link>
+              <Link className={styles.panel__link} href={"/careers"}>Careers</Link>
+              <Link className={styles.panel__link} href={"/blog"}>Blog</Link>
+              <Link className={styles.panel__link} href={"/about"}>About Us</Link>
             </nav>
-            <div className="">
-              <BtnDefault text="Login"/>
-              <BtnDefault text="Sign Up" />
+            <div className={styles.panel__auth}>
+              <BtnDefault click={onLogin} text="Login" />
+              <BtnDefault click={onSignIn} text="Sign Up" />
             </div>
           </div>
         </div>
