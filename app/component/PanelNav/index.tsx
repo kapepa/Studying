3cdf35b36cd8109f8ddb877ffa29f5257ca53f0b
kapepa <kapepayx@gmail.com>
2023-05-06 17:15:56 +0300

    app: Presentation

--- app/component/PanelNav/index.tsx
+++ app/component/PanelNav/index.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import Link from "next/link";
-import React, {FC, useCallback, useContext, useEffect, useState} from "react";
+import React, {FC, useContext, useEffect, useState} from "react";
 import BtnDefault from "../BtnDefault";
 import styles from "./styles.module.scss";
 import {BodyLayout} from "../../core/layouts/default";
@@ -11,8 +11,6 @@ const PanelNav: FC = () => {
   const mobileBtnCLass = classNames({[styles['panel__hamburger--x']]: nav});
   const mobileMobileCLass = classNames({[styles['panel__position--open']]: nav});
 
-  let windowClose = useCallback(() => bodyLayout.bodyClick(() => setNav(!nav)), [nav, bodyLayout.bodyClick])
-
   const onLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log("Login")
   }
@@ -22,43 +20,39 @@ const PanelNav: FC = () => {
   }
 
   const onMobileMenu = (e: React.MouseEvent<HTMLDivElement>) => {
-    setNav(!nav);
-
+    e.stopPropagation();
+    bodyLayout.bodyClick(() => setNav(!nav));
   }
 
   useEffect(() => {
-    // if(bodyLayout.bodyClick && nav) setNav(false);
     if( nav ) setNav(!nav);
-    console.log(nav, bodyLayout.bodyClick)
-  }, [windowClose])
+  }, [bodyLayout.bodyClick]);
 
   return (
     <div className={styles.panel}>
       <div className="container">
         <div className={styles.panel__toolbar}>
-          <Link href="/home">
+          <Link className={styles['panel__login-link']} href="/home">
             <div className={styles.panel__login}/>
           </Link>
-          <div className={styles.panel__interaction}>
-            <div className={`${styles.panel__position} ${mobileMobileCLass}`}>
-              <nav className={styles.panel__nav}>
-                <Link className={styles.panel__link} href={"/home"}>Home</Link>
-                <Link className={styles.panel__link} href={"/courses"}>Courses</Link>
-                <Link className={styles.panel__link} href={"/careers"}>Careers</Link>
-                <Link className={styles.panel__link} href={"/blog"}>Blog</Link>
-                <Link className={styles.panel__link} href={"/about"}>About Us</Link>
-              </nav>
-            </div>
-            <div className={styles.panel__auth}>
-              <BtnDefault click={onLogin} text="Login" />
-              <BtnDefault click={onSignIn} text="Sign Up" />
-            </div>
-            <div className={styles.panel__mobile}>
-              <div className={`${styles.panel__hamburger} ${mobileBtnCLass}`} onClick={onMobileMenu}>
-              	<span className={styles['panel__hamburger-row']}/>
-		            <span className={styles['panel__hamburger-row']}/>
-		            <span className={styles['panel__hamburger-row']}/>
-              </div>
+          <div className={`${styles.panel__position} ${mobileMobileCLass}`}>
+            <nav className={styles.panel__nav}>
+              <Link className={styles.panel__link} href={"/home"}>Home</Link>
+              <Link className={styles.panel__link} href={"/courses"}>Courses</Link>
+              <Link className={styles.panel__link} href={"/careers"}>Careers</Link>
+              <Link className={styles.panel__link} href={"/blog"}>Blog</Link>
+              <Link className={styles.panel__link} href={"/about"}>About Us</Link>
+            </nav>
+          </div>
+          <div className={styles.panel__auth}>
+            <BtnDefault click={onLogin} text="Login" />
+            <BtnDefault click={onSignIn} text="Sign Up" />
+          </div>
+          <div className={styles.panel__mobile}>
+            <div className={`${styles.panel__hamburger} ${mobileBtnCLass}`} onClick={onMobileMenu}>
+              <span className={styles['panel__hamburger-row']}/>
+              <span className={styles['panel__hamburger-row']}/>
+              <span className={styles['panel__hamburger-row']}/>
             </div>
           </div>
         </div>
--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,23 +1,25 @@
-import React, { FC, ReactNode } from "react"
-import Default from "../../core/layouts/default/index"
-import PanelNav from "../../component/PanelNav/index"
+import React, { FC } from "react";
+import Default from "../../core/layouts/default/index";
+import PanelNav from "../../component/PanelNav/index";
 import styles from "./styles.module.scss";
-
+import Presentation from "../../section/presentation";
 
 const Home: FC = () => {
   return (
     <Default title="Home">
       <div className={styles.home}>
         <div className={styles.home__sheet}>
-          <PanelNav/>
-          <div className={( "container")}>
-            <main className={styles.home__main}>
-              <div className="">
-                Home
-              </div>
-            </main>
-          </div>
-      
+          <picture>
+            <source srcSet="/image/girl-small.png" media="(max-width: 768px)" />
+            <source srcSet="/image/girl-middle.png" media="(max-width: 1024px)" />
+            <img className={styles['home__img-bg']} src="/image/girl-large.png" alt='girl'/>
+          </picture>
+        </div>
+        <PanelNav/>
+        <div className="container h100">
+          <main className={styles.home__main}>
+            <Presentation/>
+          </main>
         </div>
       </div>
     </Default>
