9759ae3d8681777ed01537c80bac767d5cb5efec
kapepa <kapepayx@gmail.com>
2023-05-25 23:53:46 +0300

    app: feature/payment | Acquisition

--- app/section/Acquisition/index.tsx
+++ app/section/Acquisition/index.tsx
@@ -1,9 +1,11 @@
-import React, {FC, useRef, useState} from "react";
+import React, {FC, useEffect, useRef, useState} from "react";
 import style from "./style.module.scss";
 import {useForm} from "react-hook-form";
 import BtnDefault from "../../component/BtnDefault";
 import classNames from "classnames";
 import {nameType, PaymentInterface} from "../../interface/PaymentInterface";
+import InputDefault from "../../component/InputDefault";
+import {types} from "sass";
 
 interface RadioPaymentInterface{
   value: string,
@@ -14,32 +16,23 @@ interface RadioPaymentInterface{
 }
 
 const Acquisition: FC = () => {
-  const [payment, setPayment] = useState<PaymentInterface>({ cvc: 0, date: undefined, number: '', name: "paypal" })
-  const { register, control, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: {
-      name: '',
-      number: null,
-      date: null,
-      cvc: null
-    }
+  const [payment, setPayment] = useState<PaymentInterface>({ name: "paypal", number: null, date: undefined, cvc: null })
+  const { register, control, handleSubmit, formState: { errors } } = useForm<PaymentInterface>({
+    defaultValues: { name: "paypal", number: null, date: undefined, cvc: null  }
   });
-  const inputNumber = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const letter: string = e.key;
-    const length: number = letter.length;
+
+  const inputNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value: string = e.currentTarget.value;
-    const isNumber: boolean = isNaN(Number(letter));
+    const sortNumber = value.split('').filter((letter) => !isNaN(Number(letter)));
 
-    if (length === 1 && isNumber) e.preventDefault();
-    if (length === 1 && !isNumber){
-      const space = value.split("").filter(sign => sign !== '-');
-      e.preventDefault();
+    if(sortNumber.length >= 4) sortNumber.splice(4, 0, '-');
+    if(sortNumber.length >= 9) sortNumber.splice(9, 0, '-');
+    if(sortNumber.length >= 14) sortNumber.splice(14, 0, '-');
 
-      if(value.length <= 18) {
-        e.currentTarget.value = `${e.currentTarget.value}${ space.length> 0 && space.length % 4 === 0 ? '-' : '' }${letter}`
-      }
-    }
+    e.currentTarget.value = sortNumber.join('')
   }
-  const inputDate = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+  const inputDate = (e: React.ChangeEvent<HTMLInputElement>) => {
     const letter: string = e.key;
     const length: number = letter.length;
     const value: string = e.currentTarget.value;
@@ -55,6 +48,23 @@ const Acquisition: FC = () => {
       }
     }
   }
+
+  const inputCVC = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const letter: string = e.key;
+    const length: number = letter.length;
+    const value: string = e.currentTarget.value;
+    const isNumber: boolean = isNaN(Number(letter));
+
+    if (length === 1 && isNumber) e.preventDefault();
+    if (length === 1 && !isNumber){
+      e.preventDefault();
+
+      if(value.length <= 2) {
+        e.currentTarget.value = `${e.currentTarget.value}${letter}`;
+      }
+    }
+  }
+
   const changePayment = (e: React.ChangeEvent<HTMLInputElement>) => setPayment({...payment, name: e.currentTarget.value as nameType});
 
   const onSubmit = (data) => console.log(data)
@@ -83,27 +93,71 @@ const Acquisition: FC = () => {
       <h5 className={style.acquisition__h}>Checkout</h5>
       <span className={style.acquisition__subtitle}>Cart Type</span>
       <div className={style['acquisition__payment-way']}>
-        <RadioPayment change={changePayment} name={"payment"} checked={payment.name === "paypal"} value={"paypal"} src={"/svg/paypal.svg"}/>
-        <RadioPayment change={changePayment} name={"payment"} checked={payment.name === "american express"} value={"american express"} src={"/svg/american express.svg"}/>
-        <RadioPayment change={changePayment} name={"payment"} checked={payment.name === "visa"} value={"visa"} src={"/svg/visa.svg"}/>
-        <RadioPayment change={changePayment} name={"payment"} checked={payment.name === "mastercard"} value={"mastercard"} src={"/svg/mastercard.svg"}/>
+        <RadioPayment
+          change={changePayment}
+          name={"payment"}
+          checked={payment.name === "paypal"}
+          value={"paypal"}
+          src={"/svg/paypal.svg"}
+        />
+        <RadioPayment
+          change={changePayment}
+          name={"payment"}
+          checked={payment.name === "american express"}
+          value={"american express"}
+          src={"/svg/american express.svg"}
+        />
+        <RadioPayment
+          change={changePayment}
+          name={"payment"}
+          checked={payment.name === "visa"}
+          value={"visa"}
+          src={"/svg/visa.svg"}
+        />
+        <RadioPayment
+          change={changePayment}
+          name={"payment"}
+          checked={payment.name === "mastercard"}
+          value={"mastercard"}
+          src={"/svg/mastercard.svg"}
+        />
       </div>
+      <InputDefault
+        id={'number'}
+        label={'number'}
+        type={'text'}
+        title={'Card Number'}
+        errors={errors}
+        register={register}
+        isChange={inputNumber}
+        required={'number card is required'}
+        minLength={{value: 19, message: 'this card number is short'}}
+        maxLength={{value: 19, message: 'this card number is long'}}
+      />
       <div>
-        <label htmlFor={"number"}>Card Number</label>
-        <input id={"number"} onKeyDown={inputNumber} {...register('number', { required: true, maxLength: 19})} />
-        {errors.number && <p>Number is required.</p>}
-      </div>
-      <div>
-        <div>
-          <label htmlFor={"date"}>Expiration Date ( MM/YY )</label>
-          <input id={"date"} onKeyDown={inputDate} {...register('date', { pattern: /\d+/ })} />
-          {errors.date && <p>Please enter date.</p>}
-        </div>
-        <div>
-          <label htmlFor={"cvc"}>CVC</label>
-          <input id={"cvc"} {...register('cvc', { required: true, maxLength: 3, pattern: /\d+/ })} />
-          {errors.cvc && <p>Please enter cvc.</p>}
-        </div>
+        <InputDefault
+          id={'date'}
+          label={'date'}
+          type={'text'}
+          title={'Expiration Date ( MM/YY )'}
+          errors={errors}
+          register={register}
+          isChange={inputDate}
+          required={'date is required'}
+          minLength={{value: 5, message: 'this number card is short'}}
+          maxLength={{value: 5, message: 'this date number is long'}}
+        />
+        {/*<InputDefault*/}
+        {/*  id={'cvc'}*/}
+        {/*  label={'cvc'}*/}
+        {/*  type={'text'}*/}
+        {/*  title={'CVC'}*/}
+        {/*  errors={errors}*/}
+        {/*  register={register}*/}
+        {/*  isKeyDown={inputCVC}*/}
+        {/*  required={'cvc is required'}*/}
+        {/*  minLength={{value: 3, message: 'this cvc is short'}}*/}
+        {/*/>*/}
       </div>
       <BtnDefault text={"Confirm Payment"} shape={"rounding"} type={"submit"} bg={"light-green"}/>
     </form>
