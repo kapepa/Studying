6ac102aa6719c4239739afa120b2ce0c32d18fab
kapepa <kapepayx@gmail.com>
2023-05-26 19:21:17 +0300

    app: feature/payment | Education

--- app/section/Education/index.tsx
+++ app/section/Education/index.tsx
@@ -5,9 +5,34 @@ const Education: FC = () => {
   return <section className={style.education}>
     <h5 className='h-bold'>Top Education offers and deals are listed here</h5>
     <div className={style.education__deals}>
-      <div className={style.education__deal}>
 
+      <div className={style.education__deal}>
+        <img className={style.education__bg} src={'/image/news-one.png'} alt={"img"}/>
+        <div className={style.education__inner}>
+          <div className={style.education__square}>50%</div>
+          <h6 className={style.education__h}>Lorem ipsum dolor</h6>
+          <span className={style.education__span}>Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor</span>
+        </div>
       </div>
+
+      <div className={style.education__deal}>
+        <img className={style.education__bg} src={'/image/news-two.png'} alt={"img"}/>
+        <div className={style.education__inner}>
+          <div className={style.education__square}>10%</div>
+          <h6 className={style.education__h}>Lorem ipsum dolor</h6>
+          <span className={style.education__span}>Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor</span>
+        </div>
+      </div>
+
+      <div className={style.education__deal}>
+        <img className={style.education__bg} src={'/image/news-four.png'} alt={"img"}/>
+        <div className={style.education__inner}>
+          <div className={style.education__square}>50%</div>
+          <h6 className={style.education__h}>Lorem ipsum dolor</h6>
+          <span className={style.education__span}>Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor</span>
+        </div>
+      </div>
+
     </div>
   </section>
 }
