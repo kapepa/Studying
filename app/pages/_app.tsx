98da8ce21dfe4c2745789893e86e9cd126dfb7a3
kapepa <kapepayx@gmail.com>
2023-06-04 23:51:05 +0300

    app: feature/popups | @reduxjs/toolkit

---
+++ app/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.scss'
+import React from 'react';
+import { wrapper } from "../redux/store";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default wrapper.withRedux(MyApp)
