7a326866ef068d6b6324754980dc5ad7e3cf3345
kapepa <kapepayx@gmail.com>
2023-06-24 00:42:47 +0300

    app: feature/testing | git-action

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.scss'
-import React from 'react';
+import { FC } from "react";
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
 import { wrapper } from "../redux/store";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
 
-export default wrapper.withRedux(MyApp)
+export default MyApp;
